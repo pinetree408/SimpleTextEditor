e914352a58d3c58868f1da01f63af48b4ecd86d1
leesangyoon <pinetree408@gmail.com>
2017-12-04 17:28:14 +0900

    Add first commit

---
+++ app/src/main/java/com/pinetree408/research/simpletexteditor/MainActivity.java
@@ -0,0 +1,115 @@
+package com.pinetree408.research.simpletexteditor;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.text.SpannableStringBuilder;
+import android.text.style.CharacterStyle;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        InputStream is = this.getResources().openRawResource(R.raw.test);
+        textView = (EditText) findViewById(R.id.test_text_view);
+        textView.setText(convertStreamToString(is));
+        textView.setCustomSelectionActionModeCallback(new StyleCallback());
+        textView.setInputType(InputType.TYPE_NULL);
+        textView.setSingleLine(false);
+        textView.setTextIsSelectable(true);
+    }
+
+    private String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+    class StyleCallback implements ActionMode.Callback {
+
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            System.out.println("Test Custom menu");
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.style, menu);
+            menu.removeItem(android.R.id.selectAll);
+            return true;
+        }
+
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            CharacterStyle cs;
+            int start = textView.getSelectionStart();
+            int end = textView.getSelectionEnd();
+            SpannableStringBuilder ssb = new SpannableStringBuilder(textView.getText());
+
+            switch(item.getItemId()) {
+
+                case R.id.bold:
+                    System.out.println("Test Custom menu - bold");
+                    cs = new StyleSpan(Typeface.BOLD);
+                    ssb.setSpan(cs, start, end, 1);
+                    textView.setText(ssb);
+                    return true;
+
+                case R.id.italic:
+                    System.out.println("Test Custom menu - italic");
+                    cs = new StyleSpan(Typeface.ITALIC);
+                    ssb.setSpan(cs, start, end, 1);
+                    textView.setText(ssb);
+                    return true;
+
+                case R.id.underline:
+                    System.out.println("Test Custom menu - underline");
+                    cs = new UnderlineSpan();
+                    ssb.setSpan(cs, start, end, 1);
+                    textView.setText(ssb);
+                    return true;
+            }
+            return false;
+        }
+
+        public void onDestroyActionMode(ActionMode mode) {
+        }
+    }
+}
